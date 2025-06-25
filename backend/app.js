a2c66efba633f857bd77bba2e46e683f0b086989
amishaaa129 <amishawilliams2019@gmail.com>
2025-06-25 22:15:42 +0530

    server running on port 5000

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 const cors = require('cors');
 const itemRoutes = require('./routes/items');
@@ -7,8 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 app.use('/api/items', itemRoutes);
 
 app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+  console.log(`Server running on port 5000`)
 );
